eecf1b86e381f10086e707afcb8406c2a86ee981
9485s <shivam.23036@stu.upes.ac.in>
2026-02-22 19:15:22 +0530

    Add files via upload

---
+++ dsa22.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure of Node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create linked list
+struct Node* createList(int n) {
+    struct Node *head = NULL, *temp = NULL, *newNode;
+    int x;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = x;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    return head;
+}
+
+// Function to count nodes
+int countNodes(struct Node* head) {
+    int count = 0;
+    while (head != NULL) {
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+
+// Function to print list
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node* head = createList(n);
+
+    printList(head);   // printing elements
+    printf("\nCount = %d", countNodes(head));
+
+    return 0;
+}
