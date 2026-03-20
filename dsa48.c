06a665af21c5b49eb27655f5b3021ca6ecf207db
9485s <shivam.23036@stu.upes.ac.in>
2026-03-20 23:22:58 +0530

    Add files via upload

---
+++ dsa48.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of TreeNode
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+// Function to count leaf nodes
+int countLeafNodes(struct TreeNode* root) {
+    if (root == NULL)
+        return 0;
+
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+
+    return countLeafNodes(root->left) + countLeafNodes(root->right);
+}
+
+// Helper function to create new node
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+int main() {
+
+    struct TreeNode* root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+
+    int result = countLeafNodes(root);
+    printf("Leaf Nodes = %d\n", result);
+
+    return 0;
+}
