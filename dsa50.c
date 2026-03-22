6d5d863189a7a2523b90c8e4e5351c17ed252f60
9485s <shivam.23036@stu.upes.ac.in>
2026-03-22 18:38:44 +0530

    Add files via upload

---
+++ dsa50.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of TreeNode
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+// Create a new node
+struct TreeNode* createNode(int val) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newNode->val = val;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+// Insert into BST
+struct TreeNode* insert(struct TreeNode* root, int val) {
+    if (root == NULL)
+        return createNode(val);
+
+    if (val < root->val)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+// Search in BST
+struct TreeNode* searchBST(struct TreeNode* root, int val) {
+    if (root == NULL || root->val == val)
+        return root;
+
+    if (val < root->val)
+        return searchBST(root->left, val);
+    else
+        return searchBST(root->right, val);
+}
+
+// Inorder traversal (for checking)
+void inorder(struct TreeNode* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->val);
+        inorder(root->right);
+    }
+}
+
+// Main function
+int main() {
+    struct TreeNode* root = NULL;
+
+    // Insert elements
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 60);
+    insert(root, 80);
+
+    printf("Inorder Traversal: ");
+    inorder(root);
+    printf("\n");
+
+    int val;
+    printf("Enter value to search: ");
+    scanf("%d", &val);
+
+    struct TreeNode* result = searchBST(root, val);
+
+    if (result != NULL)
+        printf("Value %d found in BST.\n", val);
+    else
+        printf("Value %d not found in BST.\n", val);
+
+    return 0;
+}
