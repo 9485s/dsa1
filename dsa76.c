580a5d0a27c78af204b241bc910baa76d1af0425
Shivam9485s <shivam.23036@stu.upes.ac.in>
2026-04-17 19:26:22 +0530

    Add files via upload

---
+++ dsa76.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 1000
+
+int visited[MAX];
+int* adj[MAX];
+int size[MAX];
+
+// DFS function
+void dfs(int node) {
+    visited[node] = 1;
+
+    for (int i = 0; i < size[node]; i++) {
+        int neighbor = adj[node][i];
+        if (!visited[neighbor]) {
+            dfs(neighbor);
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    // Initialize
+    for (int i = 1; i <= n; i++) {
+        adj[i] = (int*)malloc(n * sizeof(int));
+        size[i] = 0;
+        visited[i] = 0;
+    }
+
+    // Input edges
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+
+        adj[u][size[u]++] = v;
+        adj[v][size[v]++] = u;  // undirected graph
+    }
+
+    int components = 0;
+
+    // Count components
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+            components++;
+        }
+    }
+
+    printf("%d\n", components);
+
+    return 0;
+}
