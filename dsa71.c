b37659f7c07c7da86102fcfd51cf2465622717cf
Shivam9485s <shivam.23036@stu.upes.ac.in>
2026-04-19 23:30:48 +0530

    Add files via upload

---
+++ dsa71.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <string.h>
+
+#define EMPTY -1
+
+int hash(int key, int m) {
+    return key % m;
+}
+
+void insert(int table[], int m, int key) {
+    int i = 0;
+    int index;
+
+    while (i < m) {
+        index = (hash(key, m) + i * i) % m;
+
+        if (table[index] == EMPTY) {
+            table[index] = key;
+            return;
+        }
+        i++;
+    }
+    // Table full (optional handling)
+}
+
+int search(int table[], int m, int key) {
+    int i = 0;
+    int index;
+
+    while (i < m) {
+        index = (hash(key, m) + i * i) % m;
+
+        if (table[index] == EMPTY) {
+            return 0; // NOT FOUND
+        }
+
+        if (table[index] == key) {
+            return 1; // FOUND
+        }
+
+        i++;
+    }
+    return 0;
+}
+
+int main() {
+    int m, q;
+    scanf("%d", &m);
+
+    int table[m];
+
+    // Initialize table
+    for (int i = 0; i < m; i++) {
+        table[i] = EMPTY;
+    }
+
+    scanf("%d", &q);
+
+    while (q--) {
+        char op[10];
+        int key;
+
+        scanf("%s %d", op, &key);
+
+        if (strcmp(op, "INSERT") == 0) {
+            insert(table, m, key);
+        } 
+        else if (strcmp(op, "SEARCH") == 0) {
+            if (search(table, m, key)) {
+                printf("FOUND\n");
+            } else {
+                printf("NOT FOUND\n");
+            }
+        }
+    }
+
+    return 0;
+}
